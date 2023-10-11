e7084dbdb8e00b3f1f5d0d8487384eccbacfd77b
VicCalderoon <146136453+VicCalderoon@users.noreply.github.com>
2023-10-11 14:02:35 -0700

    Agregar archivos de proyecto.

---
+++ 3KLSC-VICCALDERON-PRAC06-PYTHON/_3KLSC_VICCALDERON_PRAC06_PYTHON.py
@@ -0,0 +1,18 @@
+
+aves = ["Loro gris, ","Paloma de diamante, ","Cóctel "]
+
+print("Los valores de la lista antes de insertar: ")
+
+for ave in aves:
+ print(ave, end=" ")
+print("\n")
+
+aves.append("Mayna, ")
+aves.append("Periquitos, ")
+aves.append("Cacatúa.")
+
+print("Los valores de la lista después de insetar: ")
+
+for ave in aves:
+ print(ave, end=" ")
+print("\n")
